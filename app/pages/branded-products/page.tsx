96014974610864a9f3b13daf4f220293d5c5f7a0
lallafm1984 <lallafm1984@gmail.com>
2025-03-27 22:19:13 +0900

    03271019

--- app/pages/branded-products/page.tsx
+++ app/pages/branded-products/page.tsx
@@ -17,7 +17,7 @@ export default function BrandedProductsPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow pt-[132px]"> {/* Header 높이만큼 상단 여백 추가 */}
+      <main className="flex-grow pt-[54px] md:pt-[100px] lg:pt-[132px] "> {/* Header 높이만큼 상단 여백 추가 */}
         <Suspense fallback={<div>Loading...</div>}>
           <BrandedProductsContent />
         </Suspense>
